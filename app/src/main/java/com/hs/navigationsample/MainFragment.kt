6eaa707d86474c9cd6fcdf506411f683d78c19f5
danledian <474608897@qq.com>
2020-04-27 17:24:25 +0800

    Initial commit

---
+++ app/src/main/java/com/hs/navigationsample/MainFragment.kt
@@ -0,0 +1,31 @@
+package com.hs.navigationsample
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_main.view.*
+
+class MainFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.bt_launch_activity_second.setOnClickListener {
+            view.findNavController().navigate(R.id.action_mainFragment_to_secondActivity)
+        }
+
+        view.bt_launch_fragment_second.setOnClickListener {
+            view.findNavController().navigate(R.id.action_mainFragment_to_secondFragment2)
+        }
+    }
+}
